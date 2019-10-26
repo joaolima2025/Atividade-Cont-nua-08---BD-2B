e0ed46f0c3d6ba3888aa5ee7eb042f1acda03b44
israelfmoraes2011 <israelfmoraes2011@hotmail.com>
2019-10-25 23:17:36 -0300

    atualizacao

--- aplicacao.py
+++ aplicacao.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
 app  = Flask(__name__)
 
-@app.route('/')
+@app.route('/index/<user>')
 
 
-def index('/index'):
-    return render_templates("index.html")
+def index(user):
+    return render_template("index.html")
 
 def cursos('/cursos'):
-    return render_templates("cursos.html")
+    return render_template("cursos.html")
 
 def Administracao('/Administracao'):
-    return render_templates("Administracao.html")
+    return render_template("Administracao.html")
 
 def Cienciasdacomptacao('/Ciências da Computação'):
-    return render_templates("Ciências da Computação.html") 
+    return render_template("Ciências da Computação.html") 
 
 def diciplina('/diciplina'):
-        return render_templates("diciplina.html")
+        return render_template("diciplina.html")
 
 def Engenharia da Computação('/Engenharia da Computação'):
-    return render_templates("Engenharia da Computação.html")   
+    return render_template("Engenharia da Computação.html")   
 
 def noticias('/noticias'):
 
-    return render_templates("noticias.html")  
+    return render_template("noticias.html")  
 
 def Sistemas de informacao('/Sistemas de informacao'):
-    return render_templates("Sistemas de informacao.html")  
+    return render_template("Sistemas de informacao.html")  
 
 app.run()
